207b0bda6ed753d1b1f5740dc3400dda8481b780
Farid Vakili <fvakili@cpp.edu>
2022-10-06 13:53:45 -0700

    Outline of TicTacToe code using comments. Provided some ideas of what functions I should use and the logic of the code.

---
+++ TicTacToe.c
@@ -0,0 +1,42 @@
+// TIC TAC TOE game between 2 players or player vs computer
+#include<stdio.h>
+#include <stdbool.h>
+
+/****FUNCTION PROTOTYPES****/
+
+//function that initializes board
+void initializeBoard();
+//function that displays board
+void displayBoard();
+//function that gets users move
+void getUserMove();
+//function that gets computer move
+void getCompMove();
+//function that switches players turn
+void swapTurn();
+//check for winner
+void checkWin();
+//check for draw
+void checkDraw();
+
+int main () {
+    //Declerations and initializations
+    
+    //prompt user game they wish to play
+
+    //read information from console
+
+    //initializeBoard
+    
+    //LOOP maybe do-while?
+    //
+    //displayBoard
+    //get move
+    //check for winner or draw
+    //END LOOP
+
+    //display winner then win state of board?
+
+
+    return 0;
+}
