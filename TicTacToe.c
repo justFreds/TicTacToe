28fb4b995e6997665df7d9fdffdf3c34de23943f
Farid Vakili <fvakili@cpp.edu>
2022-10-06 17:19:36 -0700

    Figured out how to initialze the array that displays the grid, aswell as print it out

--- TicTacToe.c
+++ TicTacToe.c
@@ -2,12 +2,17 @@
 #include<stdio.h>
 #include <stdbool.h>
 
+
+//global variable
+char grid[3][3];
+const char USERTURN = 'X';
+
 /****FUNCTION PROTOTYPES****/
 
 //function that initializes board
-void initializeBoard();
+void initializeGrid();
 //function that displays board
-void displayBoard();
+void displayGrid();
 //function that gets users move
 void getUserMove();
 //function that gets computer move
@@ -21,22 +26,42 @@ void checkDraw();
 
 int main () {
     //Declerations and initializations
-    
+
+
     //prompt user game they wish to play
 
     //read information from console
 
-    //initializeBoard
+    //initializeGrid
+    initializeGrid();
+    displayGrid();
     
     //LOOP maybe do-while?
     //
-    //displayBoard
+    //displayGrid
     //get move
     //check for winner or draw
     //END LOOP
 
     //display winner then win state of board?
 
-
     return 0;
+}
+
+void initializeGrid() {
+    int i, j;
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            grid[i][j] = ' ';
+        }
+    }
+}
+void displayGrid() {
+    for(int i = 0; i < 3; i++) {
+    printf(" %c | %c | %c ", grid[i][0], grid[i][1], grid[i][2]);
+    if(i !=2) {
+        printf("\n---|---|---\n");
+    }                       
+    }        
+    printf("\n");
 }
